01cc91273f20f72fd3ef9322903e745a7d427225
JayPee <jean.paul.bassil@outlook.com>
2024-03-09 23:47:10 +0200

    added header

---
+++ app/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-white h-24 shadow-md flex items-center justify-between px-4">
+      <h1 className="text-lg font-semibold text-gray-900">Header Title</h1>
+    </div>
+  );
+};
+
+export default Header;
