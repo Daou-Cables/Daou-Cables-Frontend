63bd4029598351bf28aebc67082b211657a0f108
Jean-Paul <jean.paul.bassil@outlook.com>
2024-03-26 00:21:56 +0200

    add: email form

--- app/ui/HomePage/ContactUs.tsx
+++ app/ui/HomePage/ContactUs.tsx
@@ -5,15 +5,19 @@ import EmailForm from "./EmailForm";
 type Props = {};
 
 export default function ContactUs({}: Props) {
-  return (
-    <div>
-      <div className="flex justify-center items-center">
-        <SectionTitle title="Contact Us" />
-      </div>
-      <div>
-        <p className="mt-12 text-lg flex items-center justify-center">Your email here for inquiries and support</p>
-      </div>
-      <EmailForm />
-    </div>
-  );
+    return (
+        <div>
+            <div className="flex justify-center items-center">
+                <SectionTitle title="Contact Us" />
+            </div>
+            <div>
+                <p className="mt-12 text-lg flex items-center justify-center mb-16">
+                    Your email here for inquiries and support
+                </p>
+            </div>
+            <div className="mb-52">
+                <EmailForm />
+            </div>
+        </div>
+    );
 }
--- app/ui/HomePage/EmailForm.tsx
+++ app/ui/HomePage/EmailForm.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import Button from './Button';
+import React from "react";
+import Button from "./Button";
+import Image from "next/image";
 
-// Assuming you have an SVG uploaded somewhere accessible via URL
-const emailIconURL = '/icons/email.svg';
+const emailIconURL = "/icons/email.svg";
 
 export default function EmailForm() {
-  return (
-    <div>
-      <form action="">
-        {/* Added styles for background SVG and text */}
-        <input
-          type="text"
-          style={{
-            backgroundImage: `url(${emailIconURL})`,
-            backgroundPosition: 'left center',
-            backgroundRepeat: 'no-repeat',
-            paddingLeft: '30px',
-          }}
-          placeholder="Your Email"
-          className='border-[1px] border-black p-3 -skew-x-12'
-        />
-        <Button title='Send Inquiry' url='/contact-us'/>
-      </form>
-    </div>
-  );
+    return (
+        <div className="w-full">
+            <form action="" className="w-full flex">
+                <div className="relative flex-grow">
+                    <input
+                        type="text"
+                        placeholder="Your Email"
+                        className="border-[1px] border-black p-3 -skew-x-12 w-full pl-16 border-r-0"
+                    />
+                    <Image
+                        src={emailIconURL}
+                        alt="email icon"
+                        style={{
+                            position: "absolute",
+                            left: "20px",
+                            top: "50%",
+                            transform: "translateY(-50%)",
+                        }}
+                        width={23}
+                        height={23}
+                    />
+                </div>
+                <Button title="Send Inquiry" url="/contact-us"/>
+            </form>
+        </div>
+    );
 }
