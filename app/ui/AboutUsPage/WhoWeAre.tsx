21f94e7e6fc5af0f537b35efb2a9564ce474af24
Jean-Paul <jean.paul.bassil@outlook.com>
2024-03-26 22:54:20 +0200

    modify: extracted imageWithText from who we are for reuisability

--- app/ui/AboutUsPage/WhoWeAre.tsx
+++ app/ui/AboutUsPage/WhoWeAre.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SectionTitle from "../General/SectionTitle";
 import Image from "next/image";
+import ImageWithText from "../General/ImageWithText";
 
 type Props = {};
 
@@ -10,26 +11,15 @@ const WhoWeAre = (props: Props) => {
             <div className="flex items-center justify-center">
                 <SectionTitle title="Who We Are" />
             </div>
-            <div className="pt-20 flex flex-col md:flex-row justify-between items-center md:px-40 space-x-0 md:space-x-20">
-                <div className="mt-8 sm:mt-12 h-full w-full md:w-1/2 md:px-0">
-                    <p className="text-xl sm:text-2xl md:text-4xl">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Sint omnis est voluptates ut minima, in blanditiis
-                        nulla qui, dignissimos a dicta earum saepe. Quos veniam
-                        dolorum fuga rem voluptatibus, fugiat quisquam minima
-                        explicabo ipsa iste dignissimos inventore obcaecati, ut
-                        numquam.
-                    </p>
-                </div>
-                <div className="flex items-center justify-center w-full md:w-1/2">
-                    <Image
-                        src="/images/WhoWeAre.png"
-                        alt="Who We Are"
-                        height={750}
-                        width={750}
-                    />
-                </div>
-            </div>
+            <ImageWithText
+                url="/images/WhoWeAre.png"
+                alt="Who We Are"
+                text="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Sint omnis est voluptates ut minima, in blanditiis nulla
+                      qui, dignissimos a dicta earum saepe. Quos veniam dolorum
+                      fuga rem voluptatibus, fugiat quisquam minima explicabo
+                      ipsa iste dignissimos inventore obcaecati, ut numquam."
+            />
         </div>
     );
 };
---
+++ app/ui/General/ImageWithText.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+
+type Props = {
+    url: string;
+    alt: string;
+    text: string;
+    reversed?: boolean;
+};
+
+function ImageWithText({ url, alt, text, reversed = false }: Props) {
+    return (
+        <div>
+            <div className="pt-20 flex flex-col md:flex-row justify-between items-center md:px-40 space-x-0 md:space-x-20">
+                <div
+                    className={`${
+                        reversed ? "hidden" : ""
+                    } mt-8 flex items-center justify-center w-full md:w-1/2`}
+                >
+                    <Image src={url} alt={alt} height={750} width={750} />
+                </div>
+                <div className="mt-8 sm:mt-12 h-full w-full md:w-1/2 md:px-0">
+                    <p className="text-xl sm:text-2xl md:text-4xl">{text}</p>
+                </div>
+                <div
+                    className={`${
+                        reversed ? "" : "hidden"
+                    } mt-8 flex items-center justify-center w-full md:w-1/2`}
+                >
+                    <Image src={url} alt={alt} height={750} width={750} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageWithText;
