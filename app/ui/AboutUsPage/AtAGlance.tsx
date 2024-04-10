b56c18dd8dd28edb701c209d540b9d0b3662f6c3
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-11 01:00:57 +0300

    styling : about us - at glance section

--- app/ui/AboutUsPage/AtAGlance.tsx
+++ app/ui/AboutUsPage/AtAGlance.tsx
@@ -5,36 +5,36 @@ import GlanceCard from "./GlanceCard";
 type Props = {};
 
 enum GlanceCardType {
-    PERCENTAGE,
-    MILLION_DOLLARS,
+  PERCENTAGE,
+  MILLION_DOLLARS,
 }
 
 const AtAGlance = (props: Props) => {
-    return (
-        <div className="pt-24">
-            <div className="flex justify-center items-center">
-                <SectionTitle title="At A Glance" />
-            </div>
-            <div className="pt-24 flex flex-col md:flex-row justify-between items-center space-y-8 md:space-y-0 md:px-64">
-                <GlanceCard
-                    number={43}
-                    type={GlanceCardType.PERCENTAGE}
-                    title="Lorem ipsum dolor sit amet."
-                />
+  return (
+    <div className="mt-32 mb-32">
+      <div className="flex justify-center items-center">
+        <SectionTitle title="At A Glance" />
+      </div>
+      <div className="pt-24 grid gap-x-2 justify-center md:grid-cols-3 md:grid-rows-1">
+        <GlanceCard
+          number={43}
+          type={GlanceCardType.PERCENTAGE}
+          title="Lorem ipsum dolor sit amet."
+        />
 
-                <GlanceCard
-                    number={84}
-                    type={GlanceCardType.PERCENTAGE}
-                    title="Lorem ipsum dolor sit amet."
-                />
-                <GlanceCard
-                    number={97.7}
-                    type={GlanceCardType.MILLION_DOLLARS}
-                    title="Lorem ipsum dolor sit amet."
-                />
-            </div>
-        </div>
-    );
+        <GlanceCard
+          number={84}
+          type={GlanceCardType.PERCENTAGE}
+          title="Lorem ipsum dolor sit amet."
+        />
+        <GlanceCard
+          number={97.7}
+          type={GlanceCardType.MILLION_DOLLARS}
+          title="Lorem ipsum dolor sit amet."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AtAGlance;
--- app/ui/AboutUsPage/GlanceCard.tsx
+++ app/ui/AboutUsPage/GlanceCard.tsx
@@ -2,61 +2,64 @@
 import React, { useState, useEffect, useRef } from "react";
 
 enum GlanceCardType {
-    PERCENTAGE,
-    MILLION_DOLLARS,
+  PERCENTAGE,
+  MILLION_DOLLARS,
 }
 
 type Props = {
-    number: number;
-    title: string;
-    type: GlanceCardType;
+  number: number;
+  title: string;
+  type: GlanceCardType;
 };
 
 const GlanceCard = ({ number, title, type }: Props) => {
-    const [displayNumber, setDisplayNumber] = useState(0);
-    const [isInView, setIsInView] = useState(false);
-    const cardRef = useRef(null);
+  const [displayNumber, setDisplayNumber] = useState(0);
+  const [isInView, setIsInView] = useState(false);
+  const cardRef = useRef(null);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            ([entry]) => setIsInView(entry.isIntersecting),
-            { threshold: 0.01 }
-        );
-
-        if (cardRef.current) {
-            observer.observe(cardRef.current);
-        }
-
-        return () => {
-            if (cardRef.current) {
-                // eslint-disable-next-line react-hooks/exhaustive-deps
-                observer.unobserve(cardRef.current);
-            }
-        };
-    }, []);
-
-    useEffect(() => {
-        if (isInView) {
-            const interval = setInterval(() => {
-                if (displayNumber < number) {
-                    setDisplayNumber((prevNumber) => prevNumber + 1);
-                }
-            }, 5);
-
-            return () => clearInterval(interval);
-        }
-    }, [displayNumber, number, isInView]);
-
-    return (
-        <div ref={cardRef} className="bg-[#f5f5f5] w-full sm:w-[400px] h-[200px] md:p-8 flex md:block flex-col items-center justify-center">
-            <h1 className="font-bold text-7xl">
-                {type === GlanceCardType.PERCENTAGE
-                    ? `${displayNumber}%`
-                    : `$${displayNumber}M`}
-            </h1>
-            <p className="mt-4 md:pl-2">{title}</p>
-        </div>
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => setIsInView(entry.isIntersecting),
+      { threshold: 0.01 }
     );
+
+    if (cardRef.current) {
+      observer.observe(cardRef.current);
+    }
+
+    return () => {
+      if (cardRef.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        observer.unobserve(cardRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isInView) {
+      const interval = setInterval(() => {
+        if (displayNumber < number) {
+          setDisplayNumber((prevNumber) => prevNumber + 1);
+        }
+      }, 5);
+
+      return () => clearInterval(interval);
+    }
+  }, [displayNumber, number, isInView]);
+
+  return (
+    <div
+      ref={cardRef}
+      className="bg-[#f5f5f5] w-full row-span-1 col-span-1 sm:max-w-[400px] h-[200px] md:p-8 flex md:block flex-col items-center justify-center"
+    >
+      <h1 className="font-bold text-7xl">
+        {type === GlanceCardType.PERCENTAGE
+          ? `${displayNumber}%`
+          : `$${displayNumber}M`}
+      </h1>
+      <p className="mt-4 md:pl-2">{title}</p>
+    </div>
+  );
 };
 
 export default GlanceCard;
