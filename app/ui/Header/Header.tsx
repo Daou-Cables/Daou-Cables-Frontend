2d8d97e2a68256b3247fd61e7d6ae8f468b24e44
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-10 15:23:25 +0300

    styling: navbar

--- app/ui/Header/Header.tsx
+++ app/ui/Header/Header.tsx
@@ -6,33 +6,31 @@ import SearchBar from "./SearchBar";
 import LanguageDropdown from "./LanguageSelect";
 
 const Header = () => {
-    return (
-        <div className="w-full bg-white shadow-md flex items-center justify-between sm:px-12 z-50 px-4 fixed">
-            <div className="flex items-center relative">
-                <Link href="/" passHref>
-                    <Image
-                        src="/daouCables.svg"
-                        alt="Logo"
-                        width={180}
-                        height={180}
-                        priority
-                    />
-                </Link>
-            </div>
+  return (
+    <div className="w-full bg-white shadow-sm flex items-center justify-between sm:px-14 z-50 px-6 fixed py-1">
+      <Link href="/" passHref>
+        <Image
+          src="/daouCables.svg"
+          alt="Daou Cables"
+          width={120}
+          height={120}
+          priority
+        />
+      </Link>
 
-            <div className="flex items-center justify-center">
-                <div className="flex flex-row text-gray-600 md:hidden">
-                    <LanguageDropdown />
-                    <Navbar />
-                </div>
-
-                <div className="hidden md:flex flex-row text-gray-600 items-center justify-center">
-                    <Navbar />
-                    <LanguageDropdown />
-                </div>
-            </div>
+      <div className="flex items-center justify-center">
+        <div className="flex flex-row text-gray-600 md:hidden">
+          <LanguageDropdown />
+          <Navbar />
         </div>
-    );
+
+        <div className="hidden md:flex flex-row text-gray-600 items-center justify-center">
+          <Navbar />
+          <LanguageDropdown />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- app/ui/Header/Menu.tsx
+++ app/ui/Header/Menu.tsx
@@ -6,75 +6,74 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 type Props = {
-    isMenuOpen: boolean;
-    setIsMenuOpen: (isOpen: boolean) => void;
+  isMenuOpen: boolean;
+  setIsMenuOpen: (isOpen: boolean) => void;
 };
 
 const tabs: Tab[] = [
-    {
-        title: "Home",
-        link: "/",
-    },
-    {
-        title: "About Us",
-        link: "/about-us",
-    },
-    {
-        title: "Products",
-        link: "/products",
-    },
-    {
-        title: "Contact Us",
-        link: "/contact-us",
-    },
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "About Us",
+    link: "/about-us",
+  },
+  {
+    title: "Products",
+    link: "/products",
+  },
+  {
+    title: "Contact Us",
+    link: "/contact-us",
+  },
 ];
 
 const getCurrentTab = (pathName: string) => {
-    const currentTab = tabs.find(tab => tab.link === pathName.toString());
-    return currentTab ? currentTab.title : "Home";
-}
+  const currentTab = tabs.find((tab) => tab.link === pathName.toString());
+  return currentTab ? currentTab.title : "Home";
+};
 
 const Menu = ({ isMenuOpen, setIsMenuOpen }: Props) => {
-    const pathName = usePathname();
-    const currentTab = getCurrentTab(pathName);
+  const pathName = usePathname();
+  const currentTab = getCurrentTab(pathName);
 
-    const [activeTab, setActiveTab] = useState(currentTab);
-    
-    const [hoveredTab, setHoveredTab] = useState(currentTab);
-    
-    return (
-        <>
-            <div className="justify-center items-center space-x-4 py-2 md:flex hidden">
-                {tabs.map((tab) => (
-                    <div key={tab.title} className="relative h-7">
-                        <Link
-                            href={tab.link}
-                            onClick={() => setActiveTab(tab.title)}
-                            className="pb-1 text-black mr-2"
-                            onMouseEnter={() => setHoveredTab(tab.title)}
-                            onMouseLeave={() => setHoveredTab("")}
-                        >
-                            {tab.title}
-                        </Link>
-                        <div
-                            className={`absolute bottom-0 w-3/5 bg-black transform -skew-x-12 transition-all duration-200 ${
-                                activeTab === tab.title ||
-                                hoveredTab === tab.title
-                                    ? "h-[2px]"
-                                    : "h-[0px]"
-                            }`}
-                        />
-                    </div>
-                ))}
-                <SearchBar />
-            </div>
-            <SideBar
-                tabs={tabs}
-                isMenuOpen={isMenuOpen}
-                setIsMenuOpen={setIsMenuOpen}
+  const [activeTab, setActiveTab] = useState(currentTab);
+  const [hoveredTab, setHoveredTab] = useState(currentTab);
+
+  return (
+    <>
+      <div className="justify-center items-center space-x-4 py-2 md:flex hidden">
+        {tabs.map((tab) => (
+          <div key={tab.title} className="relative">
+            <Link
+              href={tab.link}
+              onClick={() => setActiveTab(tab.title)}
+              className=" text-black mr-2"
+              onMouseEnter={() => setHoveredTab(tab.title)}
+              onMouseLeave={() => setHoveredTab("")}
+            >
+              {tab.title}
+            </Link>
+            <div
+              className={`absolute bottom-0 w-3/5 bg-black h-[2px] transform transition-all duration-200 ${
+                activeTab === tab.title || hoveredTab === tab.title
+                  ? "w-3/5"
+                  : "w-[0px]"
+              }`}
+              style={{ transform: "skewX(-45deg)" }}
             />
-        </>
-    );
+          </div>
+        ))}
+        <SearchBar />
+      </div>
+      <SideBar
+        tabs={tabs}
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+      />
+    </>
+  );
 };
 
 export default Menu;
